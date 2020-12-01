a03a48ac8828021182b6df39aacf246f3828f3d1
Degott <c.p.degott@tue.nl>
2020-12-01 16:36:19 +0100

    Hardcoded questions

--- Kiosk/Program.cs
+++ Kiosk/Program.cs
@@ -16,12 +16,21 @@ namespace Kiosk
             // var questions = survey.GetQuestions();
             // hard-coded for now
             var question1 = new SingleChoiceQuestion("Are you doing well?");
-            var question2 = new MultipleChoiceQuestion("What did you have for lunch?", new string[]{"Sandwitch", "Noodle", "Rice", "Nothing"});
+            var question5 = new SingleChoiceQuestion("Have you got everything ready?");
+            var question6 = new SingleChoiceQuestion("Are you ready to leave for dinner?");
+            // var question2 = new MultipleChoiceQuestion("What do you prefer for dinner?", new string[]{"Sandwich", "Noodle", "Rice", "Nothing"});
+            // var question3 = new ScoreQuestion("How would you rank your work this afternoon");
+            // var question4 = new MultipleChoiceQuestion("Which do you prefer for lunch?", new string[]{"Sandwich", "Noodle", "Rice", "Nothing"});
             var questions = new List<Question>();
             questions.Add(question1);
-            questions.Add(question2);
+            questions.Add(question5);
+            questions.Add(question6);
+            // questions.Add(question2);
+            // questions.Add(question3);
+            // questions.Add(question4);
 
             var voterAnswerMap = new SortedDictionary<string, List<Answer>>();
+
 
             // var users = getUsers();
             // hardcoded for now
@@ -36,15 +45,15 @@ namespace Kiosk
                 
                 var answers = new List<Answer>();
                 foreach (var q in questions) {
-                    var answer = q.ask();
+                    var answer = q.Ask();
                     answers.Add(answer);
                 }
                 voterAnswerMap.Add(user, answers);
             }
             
             // Create a canvas
-            YesNoDetailedResult detailedResult = new YesNoDetailedResult();
 
+            YesNoDetailedResult detailedResult = new YesNoDetailedResult();
             detailedResult.show(7, 5);
         }
     }
