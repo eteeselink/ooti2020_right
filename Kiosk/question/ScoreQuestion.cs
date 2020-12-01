91c14d11720c544a3b7901e2bda6a500ec90cac6
Degott <c.p.degott@tue.nl>
2020-12-01 15:37:04 +0100

    Updated ScoreQuestion

--- Kiosk/question/ScoreQuestion.cs
+++ Kiosk/question/ScoreQuestion.cs
@@ -14,15 +14,18 @@ namespace Kiosk
         public override Answer ask()
         {
             var response = AnsiConsole.Prompt(
-                new TextPrompt<string>(Question)
-                    .InvalidChoiceMessage("[red]Answer has to be between 1 and 5[/]")
-                    .AddChoice("1")
-                    .AddChoice("2")
-                    .AddChoice("3")
-                    .AddChoice("4")
-                    .AddChoice("5"));
+                new TextPrompt<int>(Question)
+                    .Validate(i =>
+                    {
+                        return i switch
+                        {
+                            < 1 => ValidationResult.Error("[red]Too low[/]"),
+                            > 5 => ValidationResult.Error("[red]Too high[/]"),
+                            _ => ValidationResult.Success(),
+                        };
+                    }));
 
-            return new ScoreAnswer(this, "voterName", int.Parse(response));
+            return new ScoreAnswer(this, "voterName", response);
         }
     }
 }
